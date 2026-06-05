4843f82da5a2075d24aed1ceccf164047eced205
am6nath <amarkuttan17@gmail.com>
2026-06-05 16:46:21 +0530

    authorize added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 // Program.cs
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using olx_api.Data;
 using olx_api.Extensions;
 using olx_api.Hubs;
@@ -20,7 +21,32 @@ builder.Services.AddControllers()
 
 // 2. Configure Swagger API Documentation Tools
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 builder.Services.AddSignalR();
 
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION")
